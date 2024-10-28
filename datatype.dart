36c5fc611d2cd96f3f7bd88260a1033b75a0e9c4
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-10-28 14:38:34 +0300

    datatypes

---
+++ datatype.dart
@@ -0,0 +1,24 @@
+void main() {
+  // data types in dart
+// 1. Integers
+  int amount1 = 200;
+  int amount2 = 300;
+  print('amount1:  $amount1');
+  print('amount2:  $amount2');
+
+//double
+  double amount3 = 200.0;
+  double amount4 = 300.0;
+  print('amount3:  $amount3');
+  print('amount4:  $amount4');
+
+// string
+  String name = 'John';
+  String name2 = "mike";
+  print('name:  $name');
+  print('name2:  $name2');
+
+  
+
+
+}
