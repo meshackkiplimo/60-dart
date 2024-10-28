bba48320153e24c24fcc6f74051465fb29a927b9
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-10-28 21:14:40 +0300

    intergers as input

--- input.dart
+++ input.dart
@@ -7,3 +7,14 @@ void main() {
   //string interpollation
   print('My name is $name');
 }
+
+
+// taking interger as an input value
+
+import 'dart:io';
+
+void main(){
+  stdout.writeln("what is your age: ?");
+  int? age = int.parse(stdin.readLineSync()!);
+  print('your age is $age');
+}
