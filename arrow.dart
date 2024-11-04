26271812a8c8803fb12af1e460594780c0638693
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-11-04 19:59:49 +0800

    arrow 2

--- arrow.dart
+++ arrow.dart
@@ -1,11 +1,17 @@
 // arrow functions in dart
 
-void main() {
-  // normal function
+// void main() {
+//   // normal function
+//   perim(12, 12);
+// }
+
+// void perim(int length,int width) =>
+//   print("perimeter is ${length + width}");
+
+
+void perim(int l,int w) =>
+  print("Perimeter is ${l + w}");
+
+void main(){
   perim(12, 12);
 }
-
-void perim(int length,int width) =>
-  print("perimeter is ${length + width}");
-
-
