c025f6d916ee204d5398e0b14d8a91337155a3e4
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-11-04 10:36:21 +0300

    sum of two ,,func method

---
+++ sum_func.dart
@@ -0,0 +1,10 @@
+void findSum(int num1,int num2) {
+  // int num1 = 0;
+  // int num2 = 2;
+  int? sum = num1 + num2;
+  print("the sum of the two numbers is $sum");
+}
+
+void main() {
+  findSum(8,9);
+}
